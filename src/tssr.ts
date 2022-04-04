7630e289a5f4f02f6daf7b7d5592cc82cb664762
Adenilson Prado Raul <adenilson.prado@hotmail.com>
2022-04-04 17:34:49 -0300

    fix: import in tssr.ts and version im package.json

--- src/tssr.ts
+++ src/tssr.ts
@@ -1,5 +1,6 @@
+import BuilderRunner from "./core/BuilderRunner";
 import IStrategy from "./core/IStrategy";
 import Context from "./domain/Context";
 import IEntity from "./domain/IEntity";
 
-export { IStrategy, IEntity, Context };
+export { IStrategy, IEntity, Context, BuilderRunner };
